24542fbd0d9be83c930b18eac034cd6878f675f7
toass <57165479+toass@users.noreply.github.com>
2023-01-22 17:21:28 +0900

    Create OnlyUseMainActivity.kt

---
+++ 오종현/chapter06/OnlyUseMainActivity.kt
@@ -0,0 +1,44 @@
+package com.dreamfilm.kotlintester
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        //textview
+        val name = TextView(this).apply{
+            typeface = Typeface.DEFAULT_BOLD
+            text = "오종현"
+        }
+        
+        //imageview
+        val image = ImageView(this).also{
+            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.lake_1))
+        }
+        
+        //textview
+        val address = TextView(this).apply{
+            typeface - Typeface.DEFAULT_BOLD
+            text = "이곳은 주소가 입력될 공간입니다"
+        }
+        
+        //LinearLayout 
+        val layout = LinearLayout(this).apply{
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
+            addView(name, WRAP_CONTENT, WRAP_CONTENT)
+            addView(image, WRAP_CONTENT, WRAP_CONTENT)
+            addView(address, WRAP_CONTENT, WRAP_CONTENT)
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
